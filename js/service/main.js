04d2fc5192badb3d4e25cdf4f95ee22df02921b2
renaudin-clement <pro.renaudin.clement@gmail.com>
2025-09-19 19:37:22 +0200

    mise en place d'une scene test

--- js/service/main.js
+++ js/service/main.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -16,10 +17,25 @@ const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 10;
-camera.position.x = 1;
+camera.position.y = 3;
+cube.position.y =7;
+
+const loader = new GLTFLoader();
+
+loader.load( '../../models/scene.gltf', function ( gltf ) {
+
+  scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+  console.error( error );
+
+} );
 
 function animate() {
   renderer.render( scene, camera );
+  scene.rotation.y += 0.01;
+
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
 }
