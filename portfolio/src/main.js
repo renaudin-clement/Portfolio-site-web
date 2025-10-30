4872a91bb9e8f88b91d6ff3c52b2ee7be80b87de
renaudin-clement <prof.renaudin.clement@gmail.com>
2025-10-30 19:26:38 +0100

    remaniment de vue js

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -3,3 +3,11 @@ import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
