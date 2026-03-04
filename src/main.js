291ca5187e50066ec323f85212f670100154a0c2
renaudin-clement <prof.renaudin.clement@gmail.com>
2026-03-04 16:46:59 +0100

    refactorisation

--- src/main.js
+++ src/main.js
@@ -6,44 +6,80 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 var canvas = document.querySelector('#c');
-
-var renderer = new THREE.WebGLRenderer({
-  canvas, antialias: true
- });
-
-renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
-
+var renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
 const stl = renderer.domElement.style;
-stl.top = "0px";
-stl.left = "0px";
-
-
-
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-
-
-camera.position.z = 12;
-camera.position.y = 4.0;
-cube.position.y =7;
-
 let tourn = true;
-
+const allobject = ["/models/sceneV2.gltf"];
+let assetTotal = allobject.length;
+const loaderimage= new THREE.ImageLoader();
+const allImageURLs = ["/image/one.png", "/image/two.jpg", "/image/three.gif"];
+const image = await loaderimage.loadAsync( '/image/one.png' );
+let assetCount = 0;
 const loader = new GLTFLoader();
 
-loader.load( '/models/sceneV2.gltf', function ( gltf ) {
+let cube = null;
 
-  scene.add( gltf.scene );
 
-}, undefined, function ( error ) {
+function init() {
+  renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
+  stl.top = "0px";
+  stl.left = "0px";
 
-  console.error( error );
+  const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  cube = new THREE.Mesh( geometry, material );
+  scene.add( cube );
 
-} );
+  camera.position.z = 12;
+  camera.position.y = 4.0;
+  cube.position.y =7;
+}
+
+
+init();
+
+
+
+
+
+
+
+function loadobject() {
+	for (let i = 0; i < assetTotal; i++) {
+		allobject[i] = loader.load(allobject[i], function(gltf) {
+			checkProgress();
+      scene.add( gltf.scene );
+
+    }, undefined, function ( error ) {
+
+      console.error( error );
+
+    } );
+
+	}
+}
+
+function checkProgress() {
+	assetCount ++;
+
+	if (assetCount >= assetTotal) {
+		loadingComplete();
+	}
+}
+
+// Called when all assets have finished loading
+function loadingComplete() {
+	console.log("All object are loaded!");
+	console.log(allobject);
+
+	// ... Hide preloader bar
+	// ... Start rendering scene with new assets
+}
+
+loadobject();
+
+
+
 
 
 function resizeRendererToDisplaySize(renderer) {
